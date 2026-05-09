cce8657a8799120ebe866df0c80276c49067be0e
David Broczkowski <david.broczkowski@gmail.com>
2026-05-09 11:14:14 -0400

    comment out other scripts except for acasxu

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -3,9 +3,9 @@
 TIMESTAMP="$(date -u +%Y-%m-%d_%H-%M-%S)"
 export TIMESTAMP
 
-./experiments/fairsquare/run.sh "$@"
+#./experiments/fairsquare/run.sh "$@"
 ./experiments/acasxu/run.sh "$@"
 # while the other tools we compare to scarcely log, PreimageApproxForNN
 # logs extensively, so enable logging in probspecs as well, for better comparability.
-./experiments/vcas/run.sh "$@" --log
-./experiments/mini_acs_income/run.sh "$@"
+#./experiments/vcas/run.sh "$@" --log
+#./experiments/mini_acs_income/run.sh "$@"
